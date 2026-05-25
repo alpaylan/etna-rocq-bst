57fde49cfd9d32ae26f0549c791fe89c0a129355
alpaylan <alpkeles99@gmail.com>
2026-05-25 16:00:13 -0400

    fix runner invocation, add unionunionidem

--- Runners/SpecificationBasedGenerator_test_runner.v
+++ Runners/SpecificationBasedGenerator_test_runner.v
@@ -23,6 +23,7 @@ Definition qctest_test_prop_DeleteInsert := (fun _ : unit => print_extracted_coq
 Definition qctest_test_prop_DeleteDelete := (fun _ : unit => print_extracted_coq_string ("{" ++ show (withTime(fun tt => (quickCheckWith (updMaxDiscard (updMaxSuccess (updAnalysis stdArgs true) num_tests) num_tests) test_prop_DeleteDelete))) ++ "}")).
 Definition qctest_test_prop_DeleteUnion := (fun _ : unit => print_extracted_coq_string ("{" ++ show (withTime(fun tt => (quickCheckWith (updMaxDiscard (updMaxSuccess (updAnalysis stdArgs true) num_tests) num_tests) test_prop_DeleteUnion))) ++ "}")).
 Definition qctest_test_prop_UnionDeleteInsert := (fun _ : unit => print_extracted_coq_string ("{" ++ show (withTime(fun tt => (quickCheckWith (updMaxDiscard (updMaxSuccess (updAnalysis stdArgs true) num_tests) num_tests) test_prop_UnionDeleteInsert))) ++ "}")).
+Definition qctest_test_prop_UnionUnionAssoc := (fun _ : unit => print_extracted_coq_string ("{" ++ show (withTime(fun tt => (quickCheckWith (updMaxDiscard (updMaxSuccess (updAnalysis stdArgs true) num_tests) num_tests) test_prop_UnionUnionAssoc))) ++ "}")).
 Definition qctest_test_prop_UnionUnionIdem := (fun _ : unit => print_extracted_coq_string ("{" ++ show (withTime(fun tt => (quickCheckWith (updMaxDiscard (updMaxSuccess (updAnalysis stdArgs true) num_tests) num_tests) test_prop_UnionUnionIdem))) ++ "}")).
 
 Parameter OCamlString : Type.
@@ -48,6 +49,7 @@ fun test_name ->
     (""DeleteDelete"", qctest_test_prop_DeleteDelete);
     (""DeleteUnion"", qctest_test_prop_DeleteUnion);
     (""UnionDeleteInsert"", qctest_test_prop_UnionDeleteInsert);
+    (""UnionUnionAssoc"", qctest_test_prop_UnionUnionAssoc);
     (""UnionUnionIdem"", qctest_test_prop_UnionUnionIdem)
   ] in
   let test = List.assoc test_name test_map in
@@ -59,4 +61,4 @@ Sys.argv.(1) |> qctest_map
 ".
 
 
-Extraction "SpecificationBasedGenerator_test_runner.ml" qctest_test_prop_InsertValid qctest_test_prop_DeleteValid qctest_test_prop_UnionValid qctest_test_prop_InsertPost qctest_test_prop_DeletePost qctest_test_prop_UnionPost qctest_test_prop_InsertModel qctest_test_prop_DeleteModel qctest_test_prop_UnionModel qctest_test_prop_InsertInsert qctest_test_prop_InsertDelete qctest_test_prop_InsertUnion qctest_test_prop_DeleteInsert qctest_test_prop_DeleteDelete qctest_test_prop_DeleteUnion qctest_test_prop_UnionDeleteInsert qctest_test_prop_UnionUnionIdem  qctest_map.
+Extraction "SpecificationBasedGenerator_test_runner.ml" qctest_test_prop_InsertValid qctest_test_prop_DeleteValid qctest_test_prop_UnionValid qctest_test_prop_InsertPost qctest_test_prop_DeletePost qctest_test_prop_UnionPost qctest_test_prop_InsertModel qctest_test_prop_DeleteModel qctest_test_prop_UnionModel qctest_test_prop_InsertInsert qctest_test_prop_InsertDelete qctest_test_prop_InsertUnion qctest_test_prop_DeleteInsert qctest_test_prop_DeleteDelete qctest_test_prop_DeleteUnion qctest_test_prop_UnionDeleteInsert qctest_test_prop_UnionUnionAssoc qctest_test_prop_UnionUnionIdem  qctest_map.
